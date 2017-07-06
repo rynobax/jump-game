ce0d853d12501f449b90eddd77faf614af86e53c
Ryan Baxley <rbaxley@sourcelink.com>
2017-07-06 10:34:51 -0400

    UI Updates

--- src/lobby/LobbyList.js
+++ src/lobby/LobbyList.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 import {List, ListItem} from 'material-ui/List';
 import CheckBox from 'material-ui/svg-icons/toggle/check-box';
 import CheckBoxOutline from 'material-ui/svg-icons/toggle/check-box-outline-blank';
-import Checkbox from 'material-ui/Checkbox';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class LobbyList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ready: false
+    }
+  }
+
   render() {
     const {players} = this.props;
+    let button = '';
+    if(this.state.ready) {
+      button = <RaisedButton label="Unready" secondary={true} onTouchTap={() => {
+        this.props.checkFunction(false);
+        this.setState({ready: false});
+      }} />
+    } else {
+      button = <RaisedButton label="Ready" primary={true} onTouchTap={() => {
+        this.props.checkFunction(true);
+        this.setState({ready: true});
+      }} />
+    }
     return (
       <div>
         <h3>To join, have your friends navigate to this page, and use the code to join!</h3>
@@ -17,12 +36,7 @@ class LobbyList extends Component {
             return <ListItem primaryText={name} leftIcon={ready ? <CheckBox /> : <CheckBoxOutline />} key={i}/>
           })}
         </List>
-        <Checkbox
-            label="Ready"
-            onCheck={(_, checked) => {
-              this.props.checkFunction(checked);
-            }}
-          />
+        {button}
       </div>
     )
   }
